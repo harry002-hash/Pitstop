2c2d557ec19d8135cba11a15040ff0c65bf3f5f4
Yansen-loav <Willyansen456@gmail.com>
2026-08-27 13:58:34 +0700

    wip: willyansen/frontend/login-page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [AuthenticatedSessionController::class, 'create'])
+        ->name('login');
+
+    Route::post('/login', [AuthenticatedSessionController::class, 'store']);
+});
+
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
+    ->middleware('auth')
+    ->name('logout');
+
+
+Route::get('/dashboard', function () {
+    return 'Dashboard — kamu berhasil login!';
+})->middleware('auth')->name('dashboard');

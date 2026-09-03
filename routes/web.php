84d7ce537fb2c974d8c8f99f0b67b079d4bde1d4
PWarwq <vilux456@gmail.com>
2026-09-03 14:30:47 +0700

    fix: merge conflict

--- routes/web.php
+++ routes/web.php
@@ -10,29 +10,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware('guest')->group(function () {
-    Route::get('/login', [AuthenticatedSessionController::class, 'create'])
-        ->name('login');
-
-    Route::post('/login', [AuthenticatedSessionController::class, 'store']);
-});
-
-Route::middleware('registered')->group(function () {
-    Route::get('/register', [AuthenticatedSessionController::class, 'create'])
-        ->name('register');
-
-    Route::post('/register', [AuthenticatedSessionController::class, 'store']);
-});
-
-Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
-    ->middleware('auth')
-    ->name('logout');
-
-
-Route::get('/dashboard', function () {
-    return 'Dashboard — kamu berhasil login!';
-})->middleware('auth')->name('dashboard');
-
 Route::get('login', function(){
 return view('login');
 })->name('login');
@@ -40,10 +17,6 @@ return view('login');
 Route::post('login', LoginController::class)->name('login.attempt');
 
 Route::view('dashboard', 'dashboard')->middleware('auth')->name('dashboard');
-
-Route::view('register', 'register')->name('register');
-Route::post('register', RegisterController::class)->name('register.store');
-
 
 // Chat Test
 Route::middleware('auth')->group(function () {
